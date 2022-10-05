d6dfad769f0717c67995dee6ea9943da1ed1a6bd
Nedoxy <nk.nedum@gmail.com>
2022-10-06 00:37:08 +0100

    Conditional Statements and Logical Operations

---
+++ Task_6.py
@@ -0,0 +1,36 @@
+
+# TASK 6
+# 1. Declare two numbers with any values
+# 2. Check if the first number is greater than 50 and check if the second number is less than 100,
+#    if True print out "Evaluation is True"
+# 3. Add an else block that prints "Evaluation is False"
+# 4. Declare a string with value "python"
+# 5. Check if the first string is equal to "testify" or "python", if True prints out "Yay!!
+#    The string is a Testify Python".
+
+# Note: Use the logical and to check if the first number is greater than 50 and the second number is less than 100
+
+# 1.
+score1 = 65
+score2 = 135
+
+# 2. and 3.
+if score1 > 50:
+    print("Evaluation is True")
+else:
+    print("Evaluation is False")
+
+if score2 < 100:
+    print("Evaluation is True")
+else:
+    print("Evaluation is False")
+
+# 4.
+string1 = "python"
+string2 = "testify"
+
+if string1 == string2:
+    print("Yay!! The string is a Testify Python")
+else:
+    print("No!! The string is not a Testify Python")
+
