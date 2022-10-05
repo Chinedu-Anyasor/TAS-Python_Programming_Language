c19afd8caaaac28e94c37d75bf67b9acec4c3ec9
Nedoxy <nk.nedum@gmail.com>
2022-10-06 00:30:06 +0100

    Conditional Statements and Logical Operations

---
+++ Task_5.py
@@ -0,0 +1,25 @@
+
+
+# TASK 5
+# 1. Declare a variable with value 2
+# 2. Check if the variable is equal to 2, if true print out "Number is Two"
+# 3. Add an else block which prints out "Number is not Two"
+# Note: If the value of the declared variable is changed from two, it should print "Number is not Two" in the console,
+#       if it changed back to 2 it should print "Number is Two" in the console.
+
+# 1.
+score = 2
+
+# 2.
+if score == 2:
+    print("Number is Two")
+
+# 3.
+if score == 2:
+    print("Number is Two")
+else:
+    print("Number is not Two")
+
+
+
+
