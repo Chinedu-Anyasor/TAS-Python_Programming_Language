75d32209644b1919303b900679c9792c20f7656f
Nedoxy <nk.nedum@gmail.com>
2022-10-06 00:52:37 +0100

    Arguments And Return Statement

---
+++ Task_11.py
@@ -0,0 +1,22 @@
+# TASK 11
+
+# 1. Create a function that accepts two numbers, adds the numbers and prints out the result in the console.
+# 2. Create a function that return the string value "Testify Python"
+# 3. Invoke/call the two functions
+
+def student(score1, score2):
+    total = score1 + score2
+    print("Total:", total)
+
+
+name = "Testify"
+
+
+def school():
+    program = "python"
+    print(name, program)
+
+
+student(12, 43)
+
+school()
