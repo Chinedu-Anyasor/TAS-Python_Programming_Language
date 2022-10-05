372c2de780bd81522bc40c77fae6100454f609e6
Nedoxy <nk.nedum@gmail.com>
2022-10-06 00:37:33 +0100

    Loops - For Loops and While Loops

---
+++ Task_7.py
@@ -0,0 +1,11 @@
+
+# TASK 7
+
+# 1. Use for loop to iterate from 0 to 10
+# 2. For each iteration print out "Hello World"
+
+# 1.
+number = 10
+for i in range(number):
+    print("Hello World:", i)
+
