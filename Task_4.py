38c9795a124b10f2912be9583eed0023c675dc34
Nedoxy <nk.nedum@gmail.com>
2022-10-06 00:29:36 +0100

    Basic Arithmetic Operations

---
+++ Task_4.py
@@ -0,0 +1,16 @@
+
+# TASK 4
+# 1. Declare two numbers with any values
+# 2. Add or Subtract or Multiply the two numbers together
+# 3. Print out the value of the operation
+
+# 1.
+score1 = 23
+score2 = 12
+
+# 2.
+result = score2 + score1
+
+# 3.
+print(result)
+
