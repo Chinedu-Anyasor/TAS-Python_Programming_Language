197643447051ac85273acf6a94c32b54f907e159
Nedoxy <nk.nedum@gmail.com>
2022-10-06 00:29:10 +0100

    Introduction To String and Concatenation

---
+++ Task_3.py
@@ -0,0 +1,22 @@
+
+# TASK 3
+
+# 1. Declare a raw string
+# 2. Declare two strings with any value
+# 3. Concatenate the two string
+# 4. Print out the value of the concatenated strings.
+
+# 1.
+Definition = "Populate: To have a place in\nPopulate: To furnish\nPopulate: To provide with inhabitants"
+print(Definition)
+
+# 2.
+FirstName = "Chinedu"
+LastName = "Anyasor"
+
+# 3.
+FullNames = FirstName + " " + LastName
+
+# 4.
+print(FullNames)
+
