00e4104fc71bd6111334872213cd476c95a9c7ea
Nedoxy <nk.nedum@gmail.com>
2022-10-06 00:10:53 +0100

    Data Types and Variables

---
+++ Test_2.py
@@ -0,0 +1,17 @@
+
+
+# TASK 2
+# Declare a variable with variable_name as my_var and value as 12.6
+# Declare an integer variable with the value of 200
+# Declare a String variable with the value of your name
+
+my_var = 12.6
+print(my_var)
+
+score = 200
+print(score)
+print(type(score))
+
+name = 'Chinedu' ' ' 'Anyasor'
+print(name)
+print(type(name))
