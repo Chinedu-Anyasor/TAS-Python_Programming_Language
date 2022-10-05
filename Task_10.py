e6182c08073a9e91b679cb31b75a311a1515d232
Nedoxy <nk.nedum@gmail.com>
2022-10-06 00:53:15 +0100

    Anonymous Function

---
+++ Task_10.py
@@ -0,0 +1,12 @@
+# TASK 10
+
+# 1. Create an anonymous function that prints out "Hello World"
+# 2. Invoke/call the function
+
+
+def salute():
+    print("Hello World")
+
+
+hello = lambda: print("Hello World")
+hello()
