bb7ea5450ed5e83112d92fd47e65200eb6c7f07a
Nedoxy <nk.nedum@gmail.com>
2022-10-07 23:42:45 +0100

    Basic String Operations

---
+++ Task_14.py
@@ -0,0 +1,21 @@
+# TASK
+
+# 1. Declare a string variable with any value
+# 2. Print out the string in the upper case form
+
+skill = "professionalism"
+
+upper_case = skill.upper()
+print(upper_case)
+
+size = len(skill)
+print(size)
+
+count = skill.count("s")
+print(count)
+
+stripped_name = skill.rstrip()
+print(stripped_name)
+
+
+
