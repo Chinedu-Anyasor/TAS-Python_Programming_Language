9043c111db53dd31cca53a25fdb18411efacdc91
Nedoxy <nk.nedum@gmail.com>
2022-10-07 05:27:07 +0100

    Functions and Loop Recursion

---
+++ Task_13.py
@@ -0,0 +1,14 @@
+# TASK !3
+
+# 1. Create a function that prints out "Hello World"
+# 2. Invoke the same function in it own body
+# 3. Invoke the function outside the function block
+
+
+def greet():
+    print("Hello World")
+
+
+greet()
+
+
