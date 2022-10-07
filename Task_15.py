87d71be653ebc7a593f9335a54958b2bad462be9
Nedoxy <nk.nedum@gmail.com>
2022-10-08 00:30:44 +0100

    Yaml Update

---
+++ Task_15.py
@@ -0,0 +1,16 @@
+# TASK
+
+# 1. Declare a list with any value and number of item/element
+# 2. Print the list in console
+# 3. Sort the list
+# 4. Print the list in console again
+
+countries = ["England", "Spain", "Germany", "America", "Nigeria", "Japan"]
+print(countries)
+
+countries.sort()
+print(countries)
+
+countries.sort(reverse=True)
+print(countries)
+
