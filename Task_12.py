88b0fb24d3e000f3174533a4ae24bf23a884c58f
Nedoxy <nk.nedum@gmail.com>
2022-10-07 05:20:21 +0100

    Global and Local Variables

---
+++ Task_12.py
@@ -0,0 +1,19 @@
+# TASK 12
+
+# 1. Declare a global variable with name as language and the value as "Python".
+# 2. Create a function, in the function declare a variable with name as language and value as "Java", then print out the
+#    variable in the function
+# 3. Print out the variable name into the console.
+# 4. Invoke the function
+
+# 1.
+
+language = "Python"
+
+
+def greet():
+    course = "Java"
+    print(course)
+
+
+greet()
