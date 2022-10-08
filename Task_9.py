821965aecc13aeef50dc1995fdb179eb3e6141af
Nedoxy <nk.nedum@gmail.com>
2022-10-08 23:53:19 +0100

    Functions

---
+++ Task_9.py
@@ -0,0 +1,20 @@
+# TASK 9
+
+# 1. Create a function that prints out "Hello World"
+# 2. Create a function that does nothing, an empty function
+# 3. Invoke/call both functions
+
+# 1.
+def greet():
+    print("Hello World")
+
+
+greet()
+
+
+# 2.
+def football_club():
+    pass
+
+
+football_club()
