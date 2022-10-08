352acaf532317f4de51ec662fd629775e3722166
Nedoxy <nk.nedum@gmail.com>
2022-10-08 23:50:50 +0100

    Basic Dictionary Operations

---
+++ Task_16.py
@@ -0,0 +1,20 @@
+# TASK 16
+
+# 1. Declare a dictionary with any key-value pair of items/elements
+# 2. Print the dictionary in console
+# 3. Update the dictionary with two different key-value pair items
+# 4. Print the dictionary in console again
+
+countries = {
+    "Spain": "Madrid",
+    "Nigeria": "Abuja",
+    "England": "London",
+    "Usa": "Washington Dc",
+    "Japan": "Tokyo"
+}
+print(countries)
+
+
+countries.update({"Ghana": "Accra", "Germany": "Berlin"})
+print(countries)
+
