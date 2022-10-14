86b38d5130481d31b07c47cac82aab14b740f84c
Nedoxy <nk.nedum@gmail.com>
2022-10-14 22:44:07 +0100

    Principles of OOP- Encapsulation

---
+++ Task_22.py
@@ -0,0 +1,18 @@
+# TASK 22
+
+# 1. Create a class called Hunt
+# 2. Create a private attribute called __weapon with the value "Assault Rifle" in the Hunt class
+# 3. Create a method get_weapon() that returns "Not Available" in the Hunt class
+# 4. Instantiate an object of the Hunt class
+# 5. Print the value of get_weapon() from the object instance
+
+class Hunt:
+
+    __weapon = "Assault Rifle"
+
+    def get_weapon(self):
+        return self.__weapon + " Not Available"
+
+
+hunt = Hunt()
+print(hunt.get_weapon())
