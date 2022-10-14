e0377a13b76e9b7e4b1c99087bdf4da16cb3a06e
Nedoxy <nk.nedum@gmail.com>
2022-10-14 22:28:01 +0100

    Introduction to Object-Oriented Programming

--- Task_17.py
+++ Task_17.py
@@ -5,18 +5,35 @@
 
 
 class Human:
+
     def __init__(self, name, sex, height, weight):
         self.name = name
         self.sex = sex
         self.height = height
         self.weight = weight
 
-    def details(self):
-        print(
-            "My name is " + self.name + ", " + "I am a " + self.sex + ", " + self.height + " tall" + ", " + "My weight is " + self.weight)
+    def introduce_self(self):
+        print("My name is " + self.name + ", " + "I am a " + self.sex + ", " + self.height + " tall" + ", " + "My "  "weight "  "is " + self.weight)
 
 
 student_1 = Human("Chinedu", "male", "1.68m", "68kg")
 student_2 = Human("Juliet", "female", "1.62m", "72kg")
-student_1.details()
-student_2.details()
+
+student_1.introduce_self()
+student_2.introduce_self()
+
+#personal task
+
+class Football:
+
+    def __init__(self):
+        self.country = "Spain"
+        self.club = "Fcbarcelona"
+
+    def laLiga(self):
+        return "Laliga Champions in " + self.country + "is " + self.club
+
+
+winner = Football()
+
+print(winner.laLiga())
