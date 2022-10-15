152eaced158e1cbb4b59ab745182b6520467e9b1
Nedoxy <nk.nedum@gmail.com>
2022-10-15 13:39:25 +0100

    OOP Static Methods

---
+++ Task_25.py
@@ -0,0 +1,8 @@
+# TASK 25
+
+# 1. Create a class called Utilities
+# 2. Create a static method called print_name which accepts a parameter, and prints out the parameter.
+# 3. Invoke the static method print_name()
+
+# You can use any of the two methods to create your static methods.
+
